0c6190db6559405ff331d52eb657bc243f0dd77f
Your Name <your-email@example.com>
2026-05-05 21:55:30 +0300

    d

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+#!/usr/bin/env python3
+
+def parrot(phrase=None):
+    """Prints the phrase (or "Squawk!" if no phrase given) and returns the phrase"""
+    # Determine what to print/return
+    output = phrase if phrase is not None else "Squawk!"
+    
+    # Print the output
+    print(output)
+    
+    # Return the output
+    return output
